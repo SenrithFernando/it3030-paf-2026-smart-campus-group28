ce2d6420a9a6e3520c1d4192f08e5d7431a12b99
Smethmindu <smethmindu@gmail.com>
2026-04-22 01:31:00 +0530

    feat(security): implement service to load users by email

---
+++ server/src/main/java/com/example/server/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.smartcampus.api.security;
+
+import com.smartcampus.api.model.User;
+import com.smartcampus.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + username));
+        return new CustomUserDetails(user);
+    }
+}
