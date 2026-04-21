c66822460cd79d76b73afb537b76b4c1adfe1345
Smethmindu <smethmindu@gmail.com>
2026-04-22 01:15:34 +0530

    feat(auth): add DTO for login request payloads

---
+++ server/src/main/java/com/example/server/dto/request/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.smartcampus.api.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Format must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
