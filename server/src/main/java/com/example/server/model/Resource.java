fbaba1a54025eb4c902831b4caed79aff17ddb2f
senrithfernando <rihansenrithvta@gmail.com>
2026-04-21 22:37:24 +0530

    feat(resource): implement unique constraint for resource names at the DB level

--- server/src/main/java/com/example/server/model/Resource.java
+++ server/src/main/java/com/example/server/model/Resource.java
@@ -1,6 +1,21 @@
 package com.example.server.model;
 
+import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "resources", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name", name = "uk_resource_name")
+}) // Added unique constraint
+@EntityListeners(AuditingEntityListener.class)
 public class Resource {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
