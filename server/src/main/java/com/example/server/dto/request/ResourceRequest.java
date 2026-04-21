ee6c389c1a547215d22fd7b658ba5457c30d04cc
senrithfernando <rihansenrithvta@gmail.com>
2026-04-21 23:10:39 +0530

    feat(resource): add @NotBlank with descriptive error messages to ResourceRequest

---
+++ server/src/main/java/com/example/server/dto/request/ResourceRequest.java
@@ -0,0 +1,13 @@
+package com.example.server.dto.request;
+
+public class ResourceRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Type is required")
+    private String type;
+
+    @NotBlank(message = "Location is required")
+    private String location;
+
+}
--- server/src/main/java/com/example/server/model/Resource.java
+++ server/src/main/java/com/example/server/model/Resource.java
@@ -1,11 +1,16 @@
 package com.example.server.model;
 
+import com.example.server.enums.ResourceStatus;
 import jakarta.persistence.*;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
