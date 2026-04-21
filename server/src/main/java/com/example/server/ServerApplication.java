3a578dec9b383a0728c5280a86fdb477944d5955
senrithfernando <rihansenrithvta@gmail.com>
2026-04-21 23:28:19 +0530

    feat(resource): add maven dependancies

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -1,8 +1,9 @@
 package com.example.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 
